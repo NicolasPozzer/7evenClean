37ff06afed785c38b9f7243283e6a84de92b89a3
NicolasPozzer <pozzernico@gmail.com>
2023-06-28 17:21:15 -0300

    publish-git

---
+++ 7evenClean.bat
@@ -0,0 +1,27 @@
+@echo off
+color 03
+echo Ejecutando 7evenClean By Niko7even...
+timeout 3
+cls
+color 06
+echo Realizando Limpieza y Optimizacion!
+timeout 1
+echo Realizando Limpieza y Optimizacion!
+timeout 1
+echo Realizando Limpieza y Optimizacion!
+timeout 3
+cls
+color 07
+rmdir /S /Q C:\Windows\Temp
+rmdir /S /Q C:\Windows\Prefetch
+timeout 1
+cls
+color 02
+echo Optimizacion completada!!
+timeout 1
+echo Optimizacion completada!!
+timeout 1
+echo Optimizacion completada!!
+
+pause
+exit
