97a715bd97994a48ff16e7a390cfe1368f207583
NicolasPozzer <pozzernico@gmail.com>
2023-07-07 18:09:08 -0300

    7evenclean-v2.0

--- 7evenClean.bat
+++ 7evenClean.bat
@@ -1,27 +1,20 @@
 @echo off
 color 03
 echo Ejecutando 7evenClean By Niko7even....
-timeout 3
-cls
 color 06
 echo Realizando Limpieza y Optimizacion!
 timeout 1
-echo Realizando Limpieza y Optimizacion!
-timeout 1
-echo Realizando Limpieza y Optimizacion!
-timeout 3
 cls
-color 07
+sc stop wuauserv
 rmdir /S /Q C:\Windows\Temp
 rmdir /S /Q C:\Windows\Prefetch
+rmdir /S /Q C:\Users\nico-\AppData\Local\Temp
+
 timeout 1
 cls
 color 02
 echo Optimizacion completada!!
 timeout 1
-echo Optimizacion completada!!
-timeout 1
-echo Optimizacion completada!!
 
 pause
 exit
